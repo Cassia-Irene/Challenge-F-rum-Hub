839a391da6c50e125972839d3e6d0fa8edb97ab6
IreneC16 <you@example.com>
2025-08-19 22:33:44 -0300

    primeiro commit

---
+++ src/main/java/com/example/Fhub/FhubApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhubApplication.class, args);
+	}
+
+}
